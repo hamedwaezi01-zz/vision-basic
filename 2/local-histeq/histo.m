0eb718d4342387a53a8962e01e3d6773c7f122ca
Hamed <hamedwaezi01@gmail.com>
2020-04-11 15:45:55 +0430

    added & fixed histo.m file

--- 2/local-histeq/histo.m
+++ 2/local-histeq/histo.m
@@ -4,10 +4,9 @@ res = zeros(1,256);
 [s,ss] = size(im);
 for i=1:s
   for j=1:ss
-    histog(round(im(i,j))+1) = [
-      res(round(im(i,j))+1) + 1];
+     res(round(im(i,j))+1) = res(round(im(i,j))+1) + 1;
   end
 end
-
+    
 end
 
